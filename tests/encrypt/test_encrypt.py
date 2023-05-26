49a2ce08c3c1f286b74e2fbaccf1553f2bfe72dc
Gilson-SR <gilsonsrodrigues87@gmail.com>
2023-05-26 15:01:33 -0300

    test: Requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,18 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    message = "Testing"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(message, None)
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(None, 0)
+
+    assert encrypt_message(message, 0) == "gnitseT"
+
+    assert encrypt_message(message, 1) == "T_gnitse"
+
+    assert encrypt_message(message, 2) == "gnits_eT"
