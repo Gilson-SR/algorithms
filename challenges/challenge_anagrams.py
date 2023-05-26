7f5d1ae652c0ce8a20b839476449275da62013f5
Gilson-SR <gilsonsrodrigues87@gmail.com>
2023-05-26 15:46:19 -0300

    feat: Requisito 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,37 @@
+def merge_sort(array):
+    if len(array) <= 1:
+        return "".join(array)
+
+    middle = len(array) // 2
+    left, right = merge_sort(array[:middle]), merge_sort(array[middle:])
+    return "".join(merge(left, right, array.copy()))
+
+
+def merge(left, right, merged):
+    left_cursor, right_cursor = 0, 0
+
+    while left_cursor < len(left) and right_cursor < len(right):
+        if left[left_cursor] < right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if first_string == "" and second_string == "":
+        return (first_string, second_string, False)
+
+    first_string_sorted = merge_sort(list(first_string.lower()))
+    second_string_sorted = merge_sort(list(second_string.lower()))
+    boolean = first_string_sorted == second_string_sorted
+    return (first_string_sorted, second_string_sorted, boolean)
