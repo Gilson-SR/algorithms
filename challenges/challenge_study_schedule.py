c292da9a15482ec0189b5445dde72ad778f1c37c
Gilson-SR <gilsonsrodrigues87@gmail.com>
2023-05-26 14:41:20 -0300

    feat: Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    counter = 0
+
+    for start, final in permanence_period:
+        if not isinstance(start, int) or not isinstance(final, int):
+            return None
+
+        if start <= target_time <= final:
+            counter += 1
+
+    return counter
